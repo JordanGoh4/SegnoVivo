df7cbc4dbef39700b0574437f0f3e914a13bb838
KaiTeng03 <e1354468@u.nus.edu>
2025-05-11 12:28:45 +0800

    a

---
+++ train_model.py
@@ -0,0 +1,5 @@
+import os
+import numpy as np #Used for numerical operations llike padding sequences
+import pandas as pd #Used to read CSV files into dataframes
+import tensorflow as tf #Frameworks to build and train machine learning model
+from tensorflow.keras.models import Sequential
