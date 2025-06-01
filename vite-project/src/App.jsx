64132b349203e9a2e32fa060e789b37a0e72a69f
JordanGoh4 <lowonmath@gmail.com>
2025-06-01 16:58:13 +0800

    Home page prototype 1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,47 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Navbar from './components/NavBar';
-import Hero from './components/Hero';
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="bg-indigo-700 text-white min-h-screen font-[Lexend]">
-      <Navbar />
-      <Hero />
+    <div class="main-container">
+        <div class="blur-circle1">
+
+        </div>
+        <div class="blur-circle2">
+
+        </div>
+      <div class="landing-page">
+        <header>
+          <div class="container">
+            <a href="#" class="logo">Your <b>Website</b></a>
+            <ul class="links">
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Work</li>
+              <li>Info</li>
+              <li>Get Started</li>
+            </ul>
+          </div>
+        </header>
+        <div class="content">
+          <div class="container">
+            <div class="info">
+              <h1>Looking For Inspiration</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit nihil ullam nesciunt quidem iste, Repellendus odit nihil</p>
+              <button>Button name</button>
+            </div>
+            <div class="image">
+              <img class="main-image" src="https://cdni.iconscout.com/illustration/premium/thumb/businessman-working-using-vr-tech-3840669-3202986.png?f=webp"/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     </>
   )
