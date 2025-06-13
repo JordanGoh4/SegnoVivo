9d0231b827371f2aa0a02e9d378024c90668418e
JordanGoh4 <lowonmath@gmail.com>
2025-06-13 23:59:08 +0800

    Added new pages and routes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,49 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import coverImage from './images/Cover_Image.png'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/home'
+import Contact from './pages/contact'
+
 import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <div class="main-container">
-        <div class="blur-circle1">
-
-        </div>
-        <div class="blur-circle2">
-
-        </div>
-      <div class="landing-page">
-        <header>
-          <div class="container">
-            <a href="#" class="logo">SegnoVivo</a>
-            <ul class="links">
-              <li>Home</li>
-              <li>About Us</li>
-              <li>Translate</li>
-              <li>Info</li>
-              <li>Download</li>
-            </ul>
-          </div>
-        </header>
-        <div class="content">
-          <div class="container">
-            <div class="info">
-              <h1>Making Communication Easier With Technology</h1>
-              <p>Utilizing machine learning and artificial intelligence to allow easier understanding of online resources be it videos or audio. With a extension that can be access with any browser, users will be able to view real-life translation with ease</p>
-              <button>Find out more!</button>
-            </div>
-            <div class="image">
-              <img class="main-image" src={coverImage}/>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/contact' element={<Contact />}/>
+      </Routes>
+    </main>
     </>
   )
 }
---
+++ vite-project/src/pages/home.jsx
@@ -0,0 +1,44 @@
+import coverImage from '../images/Cover_Image.png'
+
+function Home() {
+    return (
+        <>
+        <div class="main-container">
+            <div class="blur-circle1">
+    
+            </div>
+            <div class="blur-circle2">
+    
+            </div>
+          <div class="landing-page">
+            <header>
+              <div class="container">
+                <a href="#" class="logo">SegnoVivo</a>
+                <ul class="links">
+                  <li>Home</li>
+                  <li><a href='/'>About Us</a></li>
+                  <li>Translate</li>
+                  <li>Info</li>
+                  <li>Download</li>
+                </ul>
+              </div>
+            </header>
+            <div class="content">
+              <div class="container">
+                <div class="info">
+                  <h1>Making Communication Easier With Technology</h1>
+                  <p>Utilizing machine learning and artificial intelligence to allow easier understanding of online resources be it videos or audio. With a extension that can be access with any browser, users will be able to view real-life translation with ease</p>
+                  <button>Find out more!</button>
+                </div>
+                <div class="image">
+                  <img class="main-image" src={coverImage}/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </>
+      )
+}
+
+export default Home
