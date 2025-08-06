f320c345e2d378ff7a2efec61dc8e9621dac287e
Jordan <lowonmath@gmail.com>
2025-08-06 14:32:03 +0800

    Updated port

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ console.log('JWT_SECRET:', process.env.JWT_SECRET ? 'Set' : 'NOT SET');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
   database: process.env.DB_NAME || 'segno_vivo',
