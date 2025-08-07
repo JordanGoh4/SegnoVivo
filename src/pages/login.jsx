a1a18b20c8fad680298b7ad5de4192f8878901f5
KaiTeng03 <e1354468@u.nus.edu>
2025-08-07 16:53:07 +0800

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -69,7 +69,7 @@ function Login() {
 
           <div className="social-icon" style={{ marginBottom: '1.5rem' }}>
             <a href="https://segnovivo-c1iv.onrender.com/auth/google">
-
+            {/*<a href="http://localhost:3001/auth/google">*/}
               <img src={Google} alt="Google Login"></img>
             </a>
           </div>
