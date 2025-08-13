a05c5eddcc2e14d9b6e16ed0679ed1eb30f53a55
KaiTeng03 <e1354468@u.nus.edu>
2025-08-13 23:02:45 +0800

    Update server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,         
     clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-    callbackURL: `${BACKEND_URL}/auth/google/callback`
+    callbackURL: 'https://segnovivo-c1iv.onrender.com/auth/google/callback'
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
